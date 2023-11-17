ac61ead84ecd3375289c999afa8f108045012512
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-11-17 22:59:19 +0100

    11/17/2023 22:59 +

--- Challenge 01/main.py
+++ Challenge 01/main.py
@@ -5,19 +5,24 @@ def read_data():
   file_path = os.path.join(input_dir, "data.txt")
 
   with open(file_path) as f:
-    return f.read().split(' ')
+    return f.read().split()
   
-def unique_animals(data: list) -> list:
-  animals: list = []
+def count_animals(wordlist: str) -> int:
+  ordered_list = {}
+  for word in wordlist:
+    if word not in ordered_list:
+      ordered_list[word] = 1
+    else:
+      value = ordered_list.get(word)
+      ordered_list[word] = value + 1
+  print(ordered_list)
+  return ordered_list
 
-  for animal in data:
-    if animal not in animals:
-      animals.append(animal)
-      
-  return animals
+def report_words_ordered(words: dict) -> str:
+  report = ''.join("{!s}{}".format(key, val) for (key, val) in words.items())
 
-def count_unique_animals(animals):
-  
+  return report
 
 data = read_data()
-print(unique_animals(data))
+animals = count_animals(data)
+print(report_words_ordered(animals))
