b9fe4ffa2df4e7fbf9c471a14858612600b9aaca
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-11-23 16:04:12 +0100

    Added documentation

--- Challenge 03/main.py
+++ Challenge 03/main.py
@@ -3,6 +3,9 @@ import re
 
 # Leer archivo con los datos
 def read_data() -> str:
+  """
+  Abre y lee un archivo específico para analizar su contenido.
+  """
   input_dir = os.path.dirname(__file__)
   file_path = os.path.join(input_dir, "data.txt")
 
@@ -10,11 +13,21 @@ def read_data() -> str:
     return f.read().splitlines(False)
   
 # Extraer linea para analizarla
-def select_line(data: str, position: int) -> str:
+def select_line(data: list, position: int) -> str:
+  """
+  Recibe contenido a traves de $data en forma de lista.
+  Indica la $posicion dentro de $data
+  Devuelve solo la $posicion que se ha pedido.
+  """
   return data[position]
 
 # Extrae la parte de los rangos y añadirlos a una lista
 def find_range(line: str) -> list:
+  """
+  Recibe una cadena de texto.
+  Extrae números separados por simbolos y los añade a una lista.
+  Devuelve un diccionario con el rango mínimo y máximo.
+  """
   range_part = []
   range_part.append(re.findall(r'\b\d+\b', line))
   ranges = range_part[0]
@@ -26,6 +39,12 @@ def find_range(line: str) -> list:
 
 # Extrae la letra que debe repetirse
 def find_letter(line: str) -> str:
+  """
+  Recibe una cadena de texto.
+  Busca una sola letra de A a Z en minuscula delimitada por simbolos.
+  Convierte el resultado en una cadena.
+  Devuelve la letra encontrada
+  """
   letter_part = []
   letter_part.append(re.findall(r'\b[a-z]\b', line))
   letter = ' '.join(letter_part[0])
@@ -33,6 +52,12 @@ def find_letter(line: str) -> str:
 
 # Extrae el código a revisar
 def find_code(line: str) -> str:
+  """
+  Recibe una cadena de texto.
+  Busca una cadena dentro de la recibida que se encuentra despues de ':'
+  Convierte el resultado en una cadena.
+  Devuelve lo encontrado.
+  """
   code_part = []
   code_part.append(re.findall(r':\s(.+)', line))
   code = ' '.join(code_part[0])
@@ -40,6 +65,14 @@ def find_code(line: str) -> str:
 
 # Analiza si el codigo cumple los requisitos
 def analize_code(range: str, letter: str, code: str) -> bool:
+  """
+  Recibe los dos rangos de find_range()
+  Recibe la letra encontrada de find_letter()
+  Recibe la cadena encontrada de find_code()
+  Averigua si $letter se encuentra en $code
+    más veces que $min_range y menos que $max_range
+  Devuelve Bool segun la averiguacion.
+  """
   count = 0
   is_valid = False
 
@@ -54,6 +87,11 @@ def analize_code(range: str, letter: str, code: str) -> bool:
 
 # Devuelve una lista de códigos no validos
 def invalid_codes(data: str) -> str:
+  """
+  Recibe contenido a traves de $data en forma de lista.
+  Analiza cada linea dentro de $data y guarda la averiguacion.
+  Devuelve una lista de todos los códigos no válidos.
+  """
   all_invalids = []
   is_valid = 0
   i = 0
@@ -72,4 +110,5 @@ def invalid_codes(data: str) -> str:
 
   return all_invalids
 
+# Imprime el elemento nº 42
 print('submit ' + select_line(invalid_codes(read_data()), 41))
